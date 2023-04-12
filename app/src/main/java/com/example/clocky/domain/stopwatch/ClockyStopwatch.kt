89370bb3b49a5c6de723d0d2eff44d77f549a074
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-12 17:16:32 +0530

    refactor: add concrete impl of Stopwatch interface

---
+++ app/src/main/java/com/example/clocky/domain/stopwatch/ClockyStopwatch.kt
@@ -0,0 +1,32 @@
+package com.example.clocky.domain.stopwatch
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+
+/**
+ * An implementation of [Stopwatch].
+ */
+class ClockyStopwatch(private val intervalMillis: Long = 1) : Stopwatch {
+    private val _currentMillisStream = MutableStateFlow(0L)
+    private var isPaused = true
+    override val currentMillisStream = _currentMillisStream.asStateFlow()
+
+    override suspend fun start() {
+        coroutineScope {
+            isPaused = false
+            while (!isPaused) {
+                _currentMillisStream.value = _currentMillisStream.value + 1
+                delay(intervalMillis)
+            }
+        }
+    }
+
+    override fun pause() {
+        isPaused = true
+    }
+
+    override fun reset() {
+        isPaused = true
+        _currentMillisStream.value = 0L
+    }
+}
