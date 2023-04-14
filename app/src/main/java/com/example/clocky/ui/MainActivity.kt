0ecb6305e6f05e1ec3b2b9c97edd32ad1d43fe85
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-14 16:49:29 +0530

    refactor: use Stopwatch composable in MainActivity

--- app/src/main/java/com/example/clocky/ui/MainActivity.kt
+++ app/src/main/java/com/example/clocky/ui/MainActivity.kt
@@ -3,57 +3,25 @@ package com.example.clocky.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.wear.compose.material.MaterialTheme
-import androidx.wear.compose.material.Text
-import com.example.clocky.R
+import androidx.compose.runtime.*
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.clocky.domain.stopwatch.ClockyStopwatch
+import com.example.clocky.ui.stopwatch.StopWatchViewModel
+import com.example.clocky.ui.stopwatch.Stopwatch
+import com.example.clocky.ui.stopwatch.StopwatchViewModelFactory
 import com.example.clocky.ui.theme.ClockyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+            ClockyTheme {
+                val viewModelFactory = remember {
+                    StopwatchViewModelFactory(stopwatch = ClockyStopwatch())
+                }
+                val viewModel = viewModel<StopWatchViewModel>(factory = viewModelFactory)
+                Stopwatch(stopWatchViewModel = viewModel)
+            }
         }
     }
-}
-
-@Composable
-fun WearApp(greetingName: String) {
-    ClockyTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
-        ) {
-            Greeting(greetingName = greetingName)
-        }
-    }
-}
-
-@Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
-}
-
-@Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
-@Composable
-fun DefaultPreview() {
-    WearApp("Preview Android")
 }
