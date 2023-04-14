9e3a7b722c262f76ec6b1abdc0a51080ecf023f4
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-14 16:46:15 +0530

    refactor: add stateful overload of Stopwatch composable

--- app/src/main/java/com/example/clocky/ui/stopwatch/Stopwatch.kt
+++ app/src/main/java/com/example/clocky/ui/stopwatch/Stopwatch.kt
@@ -1,20 +1,38 @@
 package com.example.clocky.ui.stopwatch
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pause
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Stop
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.wear.compose.material.*
+
+/**
+ * A stateful implementation of Stopwatch that makes use of the [StopWatchViewModel].
+ */
+@Composable
+fun Stopwatch(stopWatchViewModel: StopWatchViewModel) {
+    val elapsedTimeString by stopWatchViewModel.currentMillisTextStream
+        .collectAsStateWithLifecycle(initialValue = "00:00:00:000")
+    val uiState by stopWatchViewModel.uiState.collectAsStateWithLifecycle()
+    Stopwatch(
+        elapsedTimeText = { elapsedTimeString },
+        onPlayButtonClick = stopWatchViewModel::start,
+        onPauseButtonClick = stopWatchViewModel::pause,
+        onStopButtonClick = stopWatchViewModel::stopAndReset,
+        isStopButtonEnabled = uiState != StopWatchViewModel.UiState.RESET,
+        isStopwatchRunning = uiState == StopWatchViewModel.UiState.RUNNING
+    )
+}
+
 
 @Composable
 fun Stopwatch(
