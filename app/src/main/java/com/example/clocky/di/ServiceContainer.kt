322342d71a50ce973673a077fd3828c42cb61843
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-20 16:14:03 +0530

    refactor: add ServiceContainer#provideMillisFormatter

--- app/src/main/java/com/example/clocky/di/ServiceContainer.kt
+++ app/src/main/java/com/example/clocky/di/ServiceContainer.kt
@@ -5,6 +5,8 @@ import com.example.clocky.domain.stopwatch.Stopwatch
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import android.app.Service
+import com.example.clocky.domain.millisformatter.ClockyMillisFormatter
+import com.example.clocky.domain.millisformatter.MillisFormatter
 
 /**
  * A DI container for dependencies that are to be injected in a [Service].
@@ -19,4 +21,9 @@ class ServiceContainer {
      * Provides a concrete implementation of a [Stopwatch].
      */
     fun provideStopwatch(): Stopwatch = ClockyStopwatch()
+
+    /**
+     * Provides a concrete implementation of [MillisFormatter].
+     */
+    fun provideMillisFormatter(): MillisFormatter = ClockyMillisFormatter()
 }
