c95cab2d541b58a5b94598e177c3e9dc58eb0189
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-20 13:59:54 +0530

    refactor: inject fields of StopwatchService using ClockyApplication

--- app/src/main/java/com/example/clocky/ui/stopwatch/StopwatchService.kt
+++ app/src/main/java/com/example/clocky/ui/stopwatch/StopwatchService.kt
@@ -4,19 +4,50 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import com.example.clocky.di.ClockyApplication
 import com.example.clocky.domain.stopwatch.ClockyStopwatch
 import com.example.clocky.domain.stopwatch.Stopwatch
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 
 /**
  * A service that provides a stopwatch.
  */
 class StopwatchService : Service() {
-    // todo inject stopwatch
-    private val stopwatch = ClockyStopwatch()
+    private lateinit var stopwatch: Stopwatch
+    private lateinit var coroutineScope: CoroutineScope
     private val stopwatchServiceBinder = StopwatchServiceBinder()
+
+    override fun onCreate() {
+        with((application as ClockyApplication).getServiceContainer()) {
+            stopwatch = provideStopwatch()
+            coroutineScope = provideCoroutineScope()
+        }
+    }
+
+    fun startStopwatch() {
+        // todo handle case where this method is called multiple times by different threads
+        coroutineScope.launch {
+            stopwatch.start()
+        }
+    }
+
+    fun pauseStopwatch() {
+        stopwatch.pause()
+    }
+
+    fun stopAndResetStopwatch() {
+        stopwatch.reset()
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_NOT_STICKY
 
     override fun onBind(intent: Intent): IBinder = stopwatchServiceBinder
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineScope.cancel()
+    }
 
     /**
      * A binder that provides access to the stopwatch instance used by this service.
