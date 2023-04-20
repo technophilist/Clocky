ad4a1b9699b4efe656a75de6be714328d1e84579
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-20 16:48:36 +0530

    test: add test for ClockyMillisFormatter

---
+++ app/src/test/java/com/example/clocky/domain/millisformatter/ClockyMillisFormatterTest.kt
@@ -0,0 +1,22 @@
+package com.example.clocky.domain.millisformatter
+
+import org.junit.Test
+
+class ClockyMillisFormatterTest{
+
+    private val millisFormatter = ClockyMillisFormatter()
+
+    @Test
+    fun `Millis formatting test`(){
+        // given a loop that contains the millis values for seconds in the range (10s..19s)
+        for(millis in 10_000L..19_000L step 1000L){
+            // when formatting the millis
+            val formattedMinutes = millisFormatter.formatMillis(millis)
+                .substring(6..7)
+            val expectedSecondsString = "${millis/1000}"
+            // the time must be correctly formatted
+            assert(formattedMinutes == expectedSecondsString)
+        }
+    }
+
+}
