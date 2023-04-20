160db466cbdf1a74d48a2e643dc33a4030e39f33
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-04-20 13:55:10 +0530

    refactor: add ClockyApplication#getServiceContainer

--- app/src/main/java/com/example/clocky/di/ClockyApplication.kt
+++ app/src/main/java/com/example/clocky/di/ClockyApplication.kt
@@ -2,9 +2,10 @@ package com.example.clocky.di
 
 import android.app.Application
 
-class ClockyApplication:Application() {
-
+class ClockyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
     }
+    
+    fun getServiceContainer() = ServiceContainer()
 }
