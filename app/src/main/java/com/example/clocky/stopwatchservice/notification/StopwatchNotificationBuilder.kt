baeaa3914d276506c81707006601799e83347907
t3chkid <54663474+t3chkid@users.noreply.github.com>
2023-05-08 19:05:10 +0530

    refactor: add StopwatchNotificationBuilder interface

---
+++ app/src/main/java/com/example/clocky/stopwatchservice/notification/StopwatchNotificationBuilder.kt
@@ -0,0 +1,15 @@
+package com.example.clocky.stopwatchservice.notification
+
+import android.app.Notification
+
+/**
+ * Interface for building notifications that are meant to show the current state of the
+ * stopwatch. Intended to be mainly used with [com.example.clocky.stopwatchservice.StopwatchService].
+ */
+interface StopwatchNotificationBuilder {
+    /**
+     * Builds a [Notification] containing the [currentTimeText] as the content text of the
+     * notification.
+     */
+    fun buildNotification(currentTimeText: String): Notification
+}
